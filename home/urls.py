75b783b63ff76d9221b901e6d0caf07da7571637
amane.toda <toda.amane@gmail.com>
2021-10-09 18:42:55 +0100

    Create a find_me page
    * uses geocoder

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about_project, name="about_project"),
+    path("find_me/", views.find_me, name="find_me"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils.timezone import now
+from geocoder import ip
 
 
 def home(request):
@@ -10,3 +11,19 @@ def home(request):
 def about_project(request):
     today_date = now()
     return render(request, "home/about_project.html", {"today_date": today_date})
+
+
+def find_me(request):
+    g = ip("me")
+    lat_long = g.latlng
+    address = [g.city, g.country]
+    full_info = g.json
+    return render(
+        request,
+        "home/find_me.html",
+        {
+            "lat_long": lat_long,
+            "address": address,
+            "full_info": full_info,
+        },
+    )
