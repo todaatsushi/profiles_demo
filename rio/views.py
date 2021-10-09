bc101abb1938dd8516b1d7d3ae8097be16c93b69
Atsushi Toda <todaatsushi0@gmail.com>
2021-10-09 16:50:46 +0100

    create about project page

--- rio/views.py
+++ rio/views.py
@@ -2,5 +2,10 @@ from django.http import HttpResponse
 
 
 def home(request):
-    html = "<h1>Hello, World!</h1"
+    html = "<h1>Hello, World!</h1><a href='/about-this-project/'>About</a>"
+    return HttpResponse(html)
+
+
+def about_project(request):
+    html = "<h1>This is a project made to learn django!</h1><a href='/'>Back home</a>"
     return HttpResponse(html)
