66522411217e132bf44c488dbcff6e18930d01d5
amane.toda <toda.amane@gmail.com>
2021-10-10 21:22:23 +0100

    Create users app and user profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
